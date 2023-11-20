295aa6ac0fc8beeabc5d82cadbba947fdb9fd5f0
Liam Pribis <jackpribis@gmail.com>
2023-11-20 19:22:13 +0000

    integrate ledstrip library, uart

---
+++ firmware/ledstrip/src/lib.rs
@@ -0,0 +1,44 @@
+#![no_std]
+
+use cortex_m::asm::delay;
+use embedded_hal::digital::v2::OutputPin;
+
+pub struct LedStrip<SD, SCLK, RCLK> {
+    sd: SD,
+    sclk: SCLK,
+    rclk: RCLK,
+}
+
+impl<SD, SCLK, RCLK, PinError> LedStrip<SD, SCLK, RCLK>
+where
+    SD: OutputPin<Error = PinError>,
+    SCLK: OutputPin<Error = PinError>,
+    RCLK: OutputPin<Error = PinError>,
+{
+    pub fn new(mut sd: SD, mut sclk: SCLK, mut rclk: RCLK) -> Result<Self, PinError> {
+        sd.set_low()?;
+        sclk.set_low()?;
+        rclk.set_low()?;
+        Ok(LedStrip { sd, sclk, rclk })
+    }
+
+    pub fn write_u16(&mut self, state: u16) -> Result<(), PinError> {
+        for i in 0..16 {
+            self.sclk.set_low()?;
+            self.sd.set_state((state & ((0x8000 >> i) as u16) != 0).into())?;
+            // 20MHz minimum SCLK speed, 168MHz fastest clock speed = 8.4 => 9 cycles
+            delay(9);
+            self.sclk.set_high()?;
+            delay(9);
+        }
+        self.sclk.set_low()?;
+        Ok(())
+    }
+
+    pub fn latch_output(&mut self) -> Result<(), PinError> {
+        self.rclk.set_high()?;
+        delay(9);
+        self.rclk.set_low()?;
+        Ok(())
+    }
+}
--- firmware/main/src/main.rs
+++ firmware/main/src/main.rs
@@ -1,23 +1,24 @@
 #![no_main]
 #![no_std]
 
-use panic_halt as _;
-
+use core::fmt::Write;
 use cortex_m_rt::entry;
+use hal::{gpio, pac, prelude::*};
+use nb::block;
+use panic_halt as _;
+use stm32f4xx_hal as hal;
 
-use stm32f4xx_hal::{pac, prelude::*};
+use ledstrip::LedStrip;
 
-// use `main` as the entry point of this application
-// `main` is not allowed to return
 #[entry]
 fn main() -> ! {
     let p = pac::Peripherals::take().unwrap();
     let rcc = p.RCC.constrain();
     let clocks = rcc
         .cfgr
-        .use_hse(16.MHz())
-        .hclk(168.MHz())
-        .sysclk(168.MHz())
+        // .use_hse(16.MHz())
+        // .hclk(168.MHz())
+        // .sysclk(168.MHz())
         .freeze();
 
     let mut gpiob = p.GPIOB.split();
@@ -25,10 +26,39 @@ fn main() -> ! {
 
     let mut delay = p.TIM2.delay_us(&clocks);
 
+    let gpioe = p.GPIOE.split();
+    let mut leds = LedStrip::new(
+        gpioe.pe2.into_push_pull_output(),
+        gpioe.pe3.into_push_pull_output(),
+        gpioe.pe5.into_push_pull_output(),
+    )
+    .unwrap();
+
+    let tx_pin = gpiob.pb10.into_alternate();
+    let mut tx = p.USART3.tx(
+        tx_pin,
+        hal::serial::Config::default()
+            .baudrate(115200.bps())
+            .wordlength_8()
+            .parity_none(),
+        &clocks,
+    ).unwrap();
+
+    let mut i = 0;
+
     loop {
-        dbg_led.set_high();
         delay.delay_us(200_000u32);
-        dbg_led.set_low();
         delay.delay_us(200_000u32);
+
+        leds.write_u16(1 << i);
+        leds.latch_output();
+
+        // block!(rx.read()).unwrap();
+        writeln!(tx, "Test").unwrap();
+
+        i += 1;
+        if i >= 16 {
+            i = 0;
+        }
     }
 }
